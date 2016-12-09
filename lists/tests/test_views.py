d1e2ee920e93694308aa8e7927183a05136ae671
Chris Williams <chilliams@gmail.com>
2016-12-08 20:37:10 -0500

    Refactor list view to handle new item POSTs

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -40,7 +40,7 @@ class NewItemTest(TestCase):
         correct_list = List.objects.create()
 
         self.client.post(
-            '/lists/%d/add_item' % (correct_list.id, ),
+            '/lists/%d/' % (correct_list.id, ),
             data={'item_text': 'A new item for an existing list'})
 
         self.assertEqual(Item.objects.count(), 1)
@@ -53,7 +53,7 @@ class NewItemTest(TestCase):
         correct_list = List.objects.create()
 
         response = self.client.post(
-            '/lists/%d/add_item' % (correct_list.id, ),
+            '/lists/%d/' % (correct_list.id, ),
             data={'item_text': 'A new item for an existing list'})
 
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id, ))
